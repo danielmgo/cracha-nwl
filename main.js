fd3de8ff980802e3372671bb217d19683a7c7883
DANIEL M GONCALVES <102756873+danielmgo@users.noreply.github.com>
2022-04-04 18:29:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const linksSocialMidea = {
+  github: 'danielmgo',
+  instagram: 'danielmgo19',
+  youtube: 'maykbrito',
+  facebook: 'maykbrito',
+  twitter: 'DanielG67090132'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMidea[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMidea.github}`
+
+  const h1 = fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGithubProfileInfos()
